69b36c1dd418c5ead5725a45cd0a4d48d781934a
Santiago <santiude2004@gmail.com>
2025-01-19 17:18:39 -0300

    Decoraciones en pedido

--- src/main/java/GestionRectificadoraMalvinas/Model/Pedido.java
+++ src/main/java/GestionRectificadoraMalvinas/Model/Pedido.java
@@ -1,7 +1,6 @@
 package GestionRectificadoraMalvinas.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import javax.naming.ldap.PagedResultsControl;
@@ -12,16 +11,42 @@ import java.util.Date;
 @Data //Evita crear Getters and Setters
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
     private Integer id;
+
+    @Column(name = "cliente_asociado", nullable = false)
     private Cliente cliente;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_entrada", nullable = false)
     private Date fechaEntrada;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_salida", nullable = false)
     private Date fechaSalida;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_entrega_estimada", nullable = false)
     private Date fechaEntregaEstimada;
+
+    @Column(nullable = false)
     private String estado; //activo, entregado o atrasado
+
+    @Column(name = "presupuesto_asociado", nullable = false)
     private Presupuesto presupuesto;
+
+    @Column(name = "marca_auto", nullable = false)
     private String marcaAuto;
+
+    @Column(name = "modelo_auto", nullable = false)
     private String modeloAuto;
+
+    @Column(name = "numero_serie", nullable = false)
     private String numeroSerie;
+
+    @Column(nullable = false)
     private String descripcion;
 
 }
