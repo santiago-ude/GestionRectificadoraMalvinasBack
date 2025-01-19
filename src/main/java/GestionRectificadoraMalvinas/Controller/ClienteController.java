1b4944a90441445fab04c3c5048ca681067d61b7
Santiago <santiude2004@gmail.com>
2025-01-19 17:54:58 -0300

    Create ClienteController.java

---
+++ src/main/java/GestionRectificadoraMalvinas/Controller/ClienteController.java
@@ -0,0 +1,29 @@
+package GestionRectificadoraMalvinas.Controller;
+
+import GestionRectificadoraMalvinas.Service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    ClienteService CS;
+
+    //GET
+
+    //GET By ID
+
+    //POST
+
+    //PUT
+
+    //PATCH
+
+    //DELETE
+
+
+
+}
