14675c5520b9015e7a136bbb8cb10c451c30fc95
Santiago <santiude2004@gmail.com>
2025-01-19 16:31:53 -0300

    Creacion Entidad Cliente

--- src/main/java/GestionRectificadoraMalvinas/Model/Cliente.java
+++ src/main/java/GestionRectificadoraMalvinas/Model/Cliente.java
@@ -1,4 +1,23 @@
 package GestionRectificadoraMalvinas.Model;
 
+import jakarta.persistence.*;
+
 public class Cliente {
+
+    private Integer id;
+
+    private String dni;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String numero;
+
+    private String domicilio;
+
+    private String altura;
+
+    private String metodoPago;
+    
 }
