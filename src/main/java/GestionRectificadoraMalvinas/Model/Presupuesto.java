6767fe87effeefb4dd3ecfb76ed3b3aa1a12e763
Santiago <santiude2004@gmail.com>
2025-01-19 16:56:03 -0300

    Update Presupuesto.java

--- src/main/java/GestionRectificadoraMalvinas/Model/Presupuesto.java
+++ src/main/java/GestionRectificadoraMalvinas/Model/Presupuesto.java
@@ -4,11 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity //Lo transforma en una entidad para la BDD
 @Table(name = "tbl_presupuesto") //Nombre que tendra en la BDD
 @Data //Evita crear Getters and Setters
 public class Presupuesto {
 
+    private Integer id;
+    private Date fecha;
+    private Number descuento;
+    private List<Item> items;
+    private Number total;
 
 
 
