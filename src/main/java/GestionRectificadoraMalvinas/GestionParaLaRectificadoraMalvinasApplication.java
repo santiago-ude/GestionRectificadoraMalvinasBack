938f8512b960829e003308430253b676765f9fb8
Santiago <santiude2004@gmail.com>
2025-01-19 15:52:00 -0300

    Initial commit

---
+++ src/main/java/GestionRectificadoraMalvinas/GestionParaLaRectificadoraMalvinasApplication.java
@@ -0,0 +1,13 @@
+package GestionRectificadoraMalvinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionParaLaRectificadoraMalvinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionParaLaRectificadoraMalvinasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GestionRectificadoraMalvinas/GestionParaLaRectificadoraMalvinasApplicationTests.java
@@ -0,0 +1,13 @@
+package GestionRectificadoraMalvinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionParaLaRectificadoraMalvinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
