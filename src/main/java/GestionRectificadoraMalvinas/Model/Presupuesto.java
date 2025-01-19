affb5704c8706f8bed4ee55c41042f64cf4f39c3
Santiago <santiude2004@gmail.com>
2025-01-19 16:42:47 -0300

    Creacion entidad Presupuesto

---
+++ src/main/java/GestionRectificadoraMalvinas/Model/Presupuesto.java
@@ -0,0 +1,16 @@
+package GestionRectificadoraMalvinas.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity //Lo transforma en una entidad para la BDD
+@Table(name = "tbl_presupuesto") //Nombre que tendra en la BDD
+@Data //Evita crear Getters and Setters
+public class Presupuesto {
+
+
+
+
+
+}
