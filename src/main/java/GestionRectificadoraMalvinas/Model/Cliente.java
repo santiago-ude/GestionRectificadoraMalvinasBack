469345704b69ec65807498b58363113c96d95da8
Santiago <santiude2004@gmail.com>
2025-01-19 16:32:07 -0300

    Decoraciones para la entidad Cliente

--- src/main/java/GestionRectificadoraMalvinas/Model/Cliente.java
+++ src/main/java/GestionRectificadoraMalvinas/Model/Cliente.java
@@ -2,22 +2,34 @@ package GestionRectificadoraMalvinas.Model;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tbl_cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
     private Integer id;
 
+    @Column(unique = true, nullable = false)
     private String dni;
 
+    @Column(unique = true, nullable = false)
     private String nombre;
 
+    @Column(unique = true, nullable = false)
     private String apellido;
 
+    @Column(unique = true, nullable = false)
     private String numero;
 
+    @Column(unique = true, nullable = false)
     private String domicilio;
 
+    @Column(unique = true, nullable = false)
     private String altura;
 
+    @Column(name = "metodo_de_pago", unique = true, nullable = false)
     private String metodoPago;
     
 }
