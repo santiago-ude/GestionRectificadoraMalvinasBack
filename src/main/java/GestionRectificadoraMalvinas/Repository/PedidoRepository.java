906958bb4a7c4041e1617b7505936524a0921a6a
Santiago <santiude2004@gmail.com>
2025-01-19 21:45:40 -0300

    .

--- src/main/java/GestionRectificadoraMalvinas/Repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package GestionRectificadoraMalvinas.Repository;
-
-import GestionRectificadoraMalvinas.Model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-}
--- src/main/java/GestionRectificadoraMalvinas/Repository/PresupuestoRepository.java
+++
@@ -1,9 +0,0 @@
-package GestionRectificadoraMalvinas.Repository;
-
-import GestionRectificadoraMalvinas.Model.Presupuesto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PresupuestoRepository extends JpaRepository<Presupuesto, Integer> {
-}
