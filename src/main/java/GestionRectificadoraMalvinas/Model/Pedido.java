8e5c8105b51abe143ce4e580f2a21c7a37e915d7
Santiago <santiude2004@gmail.com>
2025-01-20 11:21:37 -0300

    Create Pedido.java

---
+++ src/main/java/GestionRectificadoraMalvinas/Model/Pedido.java
@@ -0,0 +1,14 @@
+package GestionRectificadoraMalvinas.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_pedido")
+public class Pedido {
+
+
+    
+
+
+}
