5858f746ffb60c5d755bd1b17927f724f4c900eb
Santiago <santiude2004@gmail.com>
2025-01-30 00:30:36 -0300

    Delete ItemService.java

--- src/main/java/GestionRectificadoraMalvinas/Service/ItemService.java
+++ src/main/java/GestionRectificadoraMalvinas/Service/ItemService.java
@@ -1,6 +1,3 @@
-//            if(newItem.getNombre() != null){aux.setNombre(newItem.getNombre());}
-//            if(newItem.getDescripcion() != null){aux.setDescripcion(newItem.getDescripcion());}
-//            if(newItem.getPrecioUnitario() != null){aux.setPrecioUnitario(newItem.getPrecioUnitario());}
 //            if(newItem.getPrecioManoObra() != null){aux.setPrecioManoObra(newItem.getPrecioManoObra());}
 //            if(newItem.getPrecioFinal() != null){aux.setPrecioFinal(newItem.getPrecioFinal());}
 //
