bfe8a8034841fafa8400b71b7349a1ae2aa6bfc8
Santiago <santiude2004@gmail.com>
2025-01-30 00:30:10 -0300

    Delete ItemController.java

--- src/main/java/GestionRectificadoraMalvinas/Controller/ItemController.java
+++ src/main/java/GestionRectificadoraMalvinas/Controller/ItemController.java
@@ -1,17 +1,3 @@
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item no encontrado o incompleto");
-//    }
-//
-//    //PATCH
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<?> patchItem(@PathVariable Long id, @RequestBody Item newItem){
-//
-//        Item aux = IS.patchItem(id,newItem);
-//        if(aux != null){
-//
-//            return ResponseEntity.noContent().build();
-//        }
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item no encontrado");
-//    }
 //
 //    //DELETE
 //    @DeleteMapping("/{id}")
