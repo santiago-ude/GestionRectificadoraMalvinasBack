53dd7e11d870dc2f8bfa18ec52af9b4bfcfe4766
Santiago <santiude2004@gmail.com>
2025-01-30 00:30:05 -0300

    Delete ItemController.java

--- src/main/java/GestionRectificadoraMalvinas/Controller/ItemController.java
+++ src/main/java/GestionRectificadoraMalvinas/Controller/ItemController.java
@@ -1,23 +1,3 @@
-//    //POST
-//    @PostMapping
-//    public ResponseEntity<?> postItem(@RequestBody Item newItem){
-//
-//        Item aux = IS.postItem(newItem);
-//        if(aux != null){
-//
-//            URI location = ServletUriComponentsBuilder
-//                    .fromCurrentRequest()
-//                    .path("/{id}")
-//                    .buildAndExpand(newItem.getId())
-//                    .toUri();
-//
-//            return ResponseEntity.created(location).build();
-//        }
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item incompleto");
-//    }
-//
-//    //PUT
-//    @PutMapping("/{id}")
 //    public ResponseEntity<?> putItem(@PathVariable Long id, @RequestBody Item newItem){
 //
 //        Item aux = IS.putItem(id,newItem);
