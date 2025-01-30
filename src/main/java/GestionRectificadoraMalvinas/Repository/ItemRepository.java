384615fa5284110f9ea34a2a54aaa1898d752afc
Santiago <santiude2004@gmail.com>
2025-01-30 00:30:17 -0300

    Delete ItemRepository.java

--- src/main/java/GestionRectificadoraMalvinas/Repository/ItemRepository.java
+++ src/main/java/GestionRectificadoraMalvinas/Repository/ItemRepository.java
@@ -1,5 +1,3 @@
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
 //
 //@Repository
 //public interface ItemRepository extends JpaRepository<Item, Long> {
