e816c614a5e1b2e61eaed237308fb520b7f0923c
Santiago <santiude2004@gmail.com>
2025-02-06 21:37:45 -0300

    Update ClienteService.java

--- src/main/java/GestionRectificadoraMalvinas/Service/ClienteService.java
+++ src/main/java/GestionRectificadoraMalvinas/Service/ClienteService.java
@@ -53,9 +53,6 @@ public class ClienteService {
      */
     public Cliente putCliente(Long id, Cliente newCliente){
 
-        if(newCliente.getAltura() != null
-                && newCliente.getDni() != null
-                && newCliente.getApellido() != null
                 && newCliente.getDomicilio() != null
                 && newCliente.getNombre() != null
                 && newCliente.getMetodoPago() != null
