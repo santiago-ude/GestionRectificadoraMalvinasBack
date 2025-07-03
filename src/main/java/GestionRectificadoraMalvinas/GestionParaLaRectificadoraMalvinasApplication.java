e5a9fd43f3945fed6dbbbb3eeb45d93fd6dd9870
Santiago <santiude2004@gmail.com>
2025-07-03 16:51:11 -0300

    Update GestionParaLaRectificadoraMalvinasApplication.java

--- src/main/java/GestionRectificadoraMalvinas/GestionParaLaRectificadoraMalvinasApplication.java
+++ src/main/java/GestionRectificadoraMalvinas/GestionParaLaRectificadoraMalvinasApplication.java
@@ -2,12 +2,28 @@ package GestionRectificadoraMalvinas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GestionParaLaRectificadoraMalvinasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionParaLaRectificadoraMalvinasApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")  // Allow all origins
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(false);
+			};
+		};
 	}
 
 }
